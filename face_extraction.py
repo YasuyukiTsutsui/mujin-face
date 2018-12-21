0f58f52c0209637d4467ef51bf270f1b2ad11e57
Tsutsui Yasuyuki <Tsutsui@Tsutsui-no-iMac.local>
2018-12-21 13:53:39 +0900

    顔抽出してface_imgフォルダに保存後，S3にも保存するためにpost_face_imgを呼び出すように変更

--- face_extraction.py
+++ face_extraction.py
@@ -2,6 +2,7 @@
 
 import cv2
 import numpy as np
+from subprocess import check_call
 
 if __name__ == "__main__":
 
@@ -9,14 +10,14 @@ if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
 
     # OpenCVに用意されている顔認識するためのxmlファイルのパス
-    cascade_path = "/Users/pop_cycle1212/anaconda3/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml"
+    cascade_path = "/anaconda3/pkgs/opencv3-3.1.0-py35_0/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml"
     # カスケード分類器の特徴量を取得する
     cascade = cv2.CascadeClassifier(cascade_path)
 
     # 顔に表示される枠の色を指定（白色）
     color = (255,255,255)
 
-    while True:
+    while(True):
 
         # 内蔵カメラから読み込んだキャプチャデータを取得
         ret, frame = cap.read()
@@ -32,14 +33,19 @@ if __name__ == "__main__":
         if len(facerect) > 0:
             for rect in facerect:
                 #顔部分を出力
-                cv2.imshow("capture",frame[rect[1]:rect[1]+rect[3], rect[0]:rect[0]+rect[2]])
+                face_img = frame[rect[1]:rect[1]+rect[3], rect[0]:rect[0]+rect[2]]
+                cv2.imshow("capture",face_img)
         else:
             print("no face")
 
         # qキーを押すとループ終了 実行画面がカレントウィンドウの時のみ
         key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
+        if key == ord('q') or len(facerect) > 0 :
             break
+
+    #PCに保存した後にS3に保存
+    cv2.imwrite('./face_img/face.png', face_img)
+    check_call(["python", "post_face_img.py", "./face_img/face.png", "face.png"])
 
     # 内蔵カメラを終了
     cap.release()
