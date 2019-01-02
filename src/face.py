948d1f6baceb8be32e3ed6b8a461899334a5f97b
AltTether <200351p@ugs.kochi-tech.ac.jp>
2019-01-03 00:44:11 +0900

    コメント追加

--- src/face.py
+++ src/face.py
@@ -14,19 +14,25 @@ class FaceDetector():
         # 顔検出
         faces = self.cascade.detectMultiScale(frame)
 
+        # 顔がなかった場合
         if len(faces) ==  0:
             print('no face')
             return None
 
         # 大きく写っている顔のみを抽出
+
+        # 顔の縦横のサイズの合計がもっとも大きい顔を
+        # 大きく写っている顔とする
         max_face_size = -1
         max_size_face_id = -1
         for i, face in enumerate(faces):
+            # 顔の縦横のサイズを算出
             face_size = face[2]+face[3]
             if max_face_size < face_size:
                 max_face_size = face_size
                 max_size_face_id = i
 
+        # 大きく写っている顔を切り取る
         face = faces[max_size_face_id]
         face_img = frame[face[1]:face[1]+face[3], face[0]:face[0]+face[2]]
         return face_img
