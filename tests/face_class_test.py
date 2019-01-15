db6c8116d6ceca1707105ec2fd530741b7c92ac3
AltTether <200351p@ugs.kochi-tech.ac.jp>
2019-01-15 17:14:08 +0900

    カメラでの撮影ができるかどうかのテストを追加

--- tests/face_class_test.py
+++ tests/face_class_test.py
@@ -1,10 +1,14 @@
+import cv2
 import unittest
 import numpy as np
 
+from contextlib import contextmanager
 from src import FaceDetector
 
 CAMERA_PORT = 0
-face_detector = FaceDetector(CAMERA_PORT)
+CASCADE = "./src/haarcascades/haarcascade_frontalface_alt2.xml"
+
+face_detector = FaceDetector(CAMERA_PORT, cascade_path=CASCADE)
 
 class FaceClassTest(unittest.TestCase):
     # 0行列(顔なし)から顔検出をして
@@ -12,3 +16,14 @@ class FaceClassTest(unittest.TestCase):
         image_frame = np.zeros([255, 255], dtype='uint8')
         detected_face = face_detector.detecting(image_frame)
         self.assertEqual(detected_face, None)
+
+    def test_capturing(self):
+        with self.assertNotRaises(cv2.error):
+            detected_face = face_detector.detecting()
+
+    @contextmanager
+    def assertNotRaises(self, exc_type):
+        try:
+            yield None
+        except exc_type:
+            raise self.failureException('{} raised'.format(exc_type.__name__))
