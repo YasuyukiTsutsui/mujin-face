0eba6bdf1cde2f0ccb31b4ef240cc3d8a10c18b8
AltTether <200351p@ugs.kochi-tech.ac.jp>
2019-01-27 15:34:27 +0900

    顔が検出されるまでレスポンスを返さない

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 import cv2
+import json
 import tempfile
 
 from src import FaceDetector, FaceRecognizer
-from flask import Flask
+from flask import Flask, Response, request, stream_with_context
 
 
 CAMERA_PORT = 0
@@ -20,21 +21,28 @@ face_recognizer = FaceRecognizer(S3_BUCKET_NAME, COLLECTION_ID, AWS_ACCESS_KEY_I
 @app.route('/')
 def main():
     # 顔の検出と切り取り
-    face_img = face_detector.detecting()
+    def generate():
+        # 顔が検出されるまで検出を続ける
+        face_img = None
+        while(True):
+            face_img = face_detector.detecting()
+            if face_img is not None:
+                break
 
-    # 一時ファイルとして保存
-    named_temporary_file = tempfile.NamedTemporaryFile()
-    extension = '.png'
-    named_temporary_file_name = named_temporary_file.name + extension
-    cv2.imwrite(named_temporary_file_name, face_img)
+        # 一時ファイルとして保存
+        named_temporary_file = tempfile.NamedTemporaryFile()
+        extension = '.png'
+        named_temporary_file_name = named_temporary_file.name + extension
+        cv2.imwrite(named_temporary_file_name, face_img)
 
-    # 顔を識別する
-    recognize_response = face_recognizer.recognizing(named_temporary_file_name)
+        # 顔を識別する
+        recognize_response = face_recognizer.recognizing(named_temporary_file_name)
 
-    # レスポンス作成
-    print(recognize_response)
+        # レスポンス作成
+        yield json.dumps(recognize_response)
+        yield '\n'
 
-    return ""
+    return Response(stream_with_context(generate()), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
